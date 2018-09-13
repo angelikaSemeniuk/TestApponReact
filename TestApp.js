ad6f77c6ee6d40bc344cceed921fd5cf4ff72ada
Anzhelika <angelika0094@gmail.com>
2018-09-13 21:55:56 +0300

    Show a list of 10 last news on page

--- TestApp.js
+++ TestApp.js
@@ -1,10 +1,29 @@
 import React from "react";
+import RequestComponent from "./RequestComponent";
+import ListComponent from "./ListComponent";
 
 export default class TestApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {arrayOfTitels: []}
+        this.updateArrayOfTitels =  this.updateArrayOfTitels.bind(this);
+    }
+
+    updateArrayOfTitels (arrayOfTitels) {
+        this.setState ({arrayOfTitels: arrayOfTitels});
+    }
+
     render () {
         return (
             <div>
-                <h1>Hello World!</h1>
+                <div className="header">
+                    <h1>The Guardian News</h1>
+                </div>
+                <RequestComponent
+                    arrayOfTitels={this.state.arrayOfTitels}
+                    updateArrayOfTitels={this.updateArrayOfTitels}
+                />
+                <ListComponent arrayOfTitels={this.state.arrayOfTitels}/>
             </div>
         );
     }
